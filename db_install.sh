b722e3847e41fb66b184c59db626995949d93fc1
Aysad Kozanoglu <aysadk@emailn.de>
2016-10-22 22:20:23 +0200

    Update db_install.sh

--- db_install.sh
+++ db_install.sh
@@ -2,10 +2,12 @@
 # This script made by sevano (fos-streaming.com)
 
 echo  "Database Installation"
-
-read -p "Choose your MySQL database name: " sqldatabase 
-read -p "Enter your MySQL username(usual 'root'): " sqluname 
-read -rep $'Enter your MySQL password (ENTER for none):' sqlpasswd
+sqlpasswd="wsxedc"
+sqldatabase="streaming"
+sqluname="root"
+#read -p "Choose your MySQL database name: " sqldatabase 
+#read -p "Enter your MySQL username(usual 'root'): " sqluname 
+#read -rep $'Enter your MySQL password (ENTER for none):' sqlpasswd
 echo "mysql-server mysql-server/root_password password $sqlpasswd" | debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password $sqlpasswd" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
@@ -23,9 +25,9 @@ mysql -uroot -p$sqlpasswd -e "grant all privileges on $sqldatabase.* to '$sqluna
 echo  "hostname: localhost, database_name: " $sqldatabase " , database_username: "  $sqluname  " , database_password " $sqlpasswd
 echo "\n "
 
-sed -i 's/xxx/'$sqldatabase'/g' /home/fos-streaming/fos/www/config.php  
-sed -i 's/zzz/'$sqlpasswd'/g' /home/fos-streaming/fos/www/config.php 
-sed -i 's/ttt/'$sqluname'/g' /home/fos-streaming/fos/www/config.php
+sed -i 's/DBNAME/'$sqldatabase'/g' /home/fos-streaming/fos/www/config.php  
+sed -i 's/DBPASS/'$sqlpasswd'/g' /home/fos-streaming/fos/www/config.php 
+sed -i 's/DBUSER/'$sqluname'/g' /home/fos-streaming/fos/www/config.php
 
 cd /usr/src/
 wget https://getcomposer.org/installer 
@@ -34,5 +36,5 @@ cd /home/fos-streaming/fos/www/
 php /usr/src/composer.phar install  
 
 curl "http://127.0.0.1:8000/install.php?install" 
-curl "http://127.0.0.1:8000/install.php?update"
-rm -r /home/fos-streaming/fos/www/install.php
+#curl "http://127.0.0.1:8000/install.php?update"
+#rm -r /home/fos-streaming/fos/www/install.php
